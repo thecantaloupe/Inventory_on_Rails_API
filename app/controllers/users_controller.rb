ebe9bbc2af1d4b076b45d51ff9bafb654d87299a
Zachary Gomez <zgomez301@gmail.com>
2022-01-08 00:37:15 -0800

    moved user creation route behind signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password)
+        params.permit(:email, :password)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :warehouses do 
     resources :inventory_levels
   end
-  resource :users, only: [:create]
-  post "/login", to: "users#login"
-  get "/auto_login", to: "users#auto_login"
+  resource :users, only: [] do
+    post "/signup", to: "users#create"
+    post "/login", to: "users#login"
+    get "/auto_login", to: "users#auto_login"
+  end
 end
